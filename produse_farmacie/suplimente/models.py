00cf852d812fd98227863dc871fc99d09b77c5bd
mariusciurea <ciurea.marius1@gmail.com>
2023-10-25 23:19:16 +0300

    errors fixed

--- produse_farmacie/suplimente/models.py
+++ produse_farmacie/suplimente/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
 class Medicamente(models.Model):
     medicament = models.CharField(max_length=100)
+    cantitate = models.CharField(max_length=100, default=0)
     comandat = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.medicament
--- produse_farmacie/suplimente/views.py
+++ produse_farmacie/suplimente/views.py
@@ -1,40 +1,45 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Medicamente
 from .forms import MedicamenteForm
+
 med = [
     {
         'id': 1,
-        'nume':'VITAMINE',
+        'nume': 'VITAMINE',
         'otc': 'DA',
-        'nr':'100'
+        'nr': '100'
     },
     {
         'id': 2,
-        'nume':'PRADAXA',
-        'otc':'NU',
-        'nr':'0'
+        'nume': 'PRADAXA',
+        'otc': 'NU',
+        'nr': '0'
     },
     {
         'id': 3,
-        'nume':'MAGNEZIU',
-        'otc':'DA',
-        'nr':'45'
+        'nume': 'MAGNEZIU',
+        'otc': 'DA',
+        'nr': '45'
     }
 ]
+
+
 def home(request):
     # return HttpResponse('<h2> Aici vor fi medicamentele</h2>')
     medicamente = Medicamente.objects.all()
     form = MedicamenteForm()
     if request.method == 'POST':
-        Medicamente.objects.create(medicament=request.POST.get('medicament'),comandat=request.POST.get('comandat') )
+        Medicamente.objects.create(
+            medicament=request.POST.get('medicament'),
+            cantitate=request.POST.get('cantitate'),
+            comandat=True if request.POST.get('comandat') == 'on' else False
+        )
         return redirect('home')
-    context = {'med': medicamente ,'form':form}
-    return render(request,'suplimente/home.html',context)
+    context = {'med': medicamente, 'form': form}
+    return render(request, 'suplimente/home.html', context)
+
 
 def about(request):
     # return HttpResponse('<h2>About my app</h2>')
-    return render (request,'suplimente/about.html')
-
-
-
+    return render(request, 'suplimente/about.html')
